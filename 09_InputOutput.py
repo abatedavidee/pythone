ca681f0b508f213ae0c58b123849a303db9aa001
Abate <abate.davidealessandro@itisgrassi.edu.it>
2025-02-14 13:49:48 +0100

    Esercitazione 9

---
+++ 09_InputOutput.py
@@ -0,0 +1,8 @@
+print("Benvenuto nel mio programma di conservazione")
+print("Ti piace andare in bicicletta")
+risposta=input("Rispondi si o no: ")
+if risposta=="si":
+    print("Molto bene! Ti terrai in forma.")
+else:
+    print("Forse ti piace qualche altro sport.")
+print("Ciao ciao.")
