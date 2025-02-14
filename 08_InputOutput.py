0e2535a3766011dd869b5241e71b9dc13895bc09
Abate <abate.davidealessandro@itisgrassi.edu.it>
2025-02-14 13:44:16 +0100

    Esercitazione 8

---
+++ 08_InputOutput.py
@@ -0,0 +1,8 @@
+print("Benvenuto nel mio programma di conversazione")
+print("Ti piace andare in bicicletta?")
+risposta=input("Rispondi si o no: ")
+if risposta=="si":
+     print ("Molto bene! Ti terrai in forma.")
+if risposta=="no":
+     print("Male male, tieniti in forma!")
+print("Ciao ciao.")
