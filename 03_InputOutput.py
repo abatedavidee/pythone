01331c73b705d2758fd23ad3b5f6676d9e651139
Abate <abate.davidealessandro@itisgrassi.edu.it>
2025-02-14 11:47:27 +0100

    Esercitazione 3

---
+++ 03_InputOutput.py
@@ -0,0 +1,9 @@
+print("Vogliamo sapere qual'è il tuo nome")
+nome=input()
+
+print("Ciao", nome)
+print()
+print("Vogliamo sapere qual'è il tuo colore preferito")
+print("Qual'è il tuo colore preferito?")
+risposta=input()
+print("Bene, mi piace anche a me", risposta)
